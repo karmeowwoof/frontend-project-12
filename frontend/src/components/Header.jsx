5e7094cfb6ba7636d22d2e3bc4348500a9ef6dae
karmeowwoof <karmeowwoof@gmail.com>
2023-01-18 16:16:19 +0300

    linter fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { useAuth } from '../hooks/index.js';
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useAuth } from "../hooks/index.js";
 
 const Header = () => {
   const auth = useAuth();
@@ -11,13 +11,15 @@ const Header = () => {
   return (
     <Navbar className="shadow-sm navbar-expand-lg navbar-light bg-white">
       <Container>
-    <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-    {auth.user ? (
-    <Link to="/" onClick={auth.logOut}>
-    {t('logout')}
-    </Link>
-    ) : null}
-    </Container>
+        <Navbar.Brand as={Link} to="/">
+          Hexlet Chat
+        </Navbar.Brand>
+        {auth.user ? (
+          <Link to="/" onClick={auth.logOut}>
+            {t("logout")}
+          </Link>
+        ) : null}
+      </Container>
     </Navbar>
   );
 };
--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -73,11 +73,11 @@ const Messages = () => {
 
   return (
     <div className="col p-0 h-100">
-  <div className="d-flex flex-column h-100">
-  <div className="bg-light mb-4 p-3 shadow-sm small">
-  <p className="m-0">
-  <b>{ `${currentChannel?.name}`}</b>
-</p>
+      <div className="d-flex flex-column h-100">
+    <div className="bg-light mb-4 p-3 shadow-sm small">
+    <p className="m-0">
+    <b>{ `${currentChannel?.name}`}</b>
+    </p>
   <span className="text-muted">
   {t('messages.counter.key', { count: currentMessages.length })}
 </span>
